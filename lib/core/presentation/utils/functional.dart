fc6c0d5981e40b3f65a9762ccaf39df94335b456
AhmedLSayed9 <ahmedelsaayid@gmail.com>
2022-12-25 22:07:24 +0200

    Upgrade rp to 2.1.2

--- lib/core/presentation/utils/functional.dart
+++ lib/core/presentation/utils/functional.dart
@@ -2,10 +2,11 @@
 /// the project. It also exports the [FpState] type alias, which is used to
 /// avoid conflicts with the [State] class from the Flutter SDK.
 import 'package:fpdart/fpdart.dart' as fpdart show State;
+
 /// The `fpdart` library is used to create functional programming constructs.
 export 'package:fpdart/fpdart.dart' hide State;
+import 'package:fpdart/fpdart.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'functional.dart';
 
 /// A type alias for the [State] class from the `fpdart` library.
 typedef FpState<S, A> = fpdart.State<S, A>;
