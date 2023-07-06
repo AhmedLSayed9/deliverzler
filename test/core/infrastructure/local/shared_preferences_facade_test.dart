1c586b32c3fccd718a5e24842a8d080bffdb055b
Ahmed Elsayed <ahmedelsaayid@gmail.com>
2023-07-06 11:18:55 +0300

    Test UnsupportedError cases for SharedPreferencesFacade

--- test/core/infrastructure/local/shared_preferences_facade_test.dart
+++ test/core/infrastructure/local/shared_preferences_facade_test.dart
@@ -23,7 +23,7 @@ void main() {
   const tBoolValue = true;
   const tStringList = ['t1', 't2'];
 
-  const tError = FormatException('error');
+  final tError = UnimplementedError('error');
 
   group('saveData', () {
     test(
@@ -33,11 +33,13 @@ void main() {
         const tResponse = true;
         when(() => mockSharedPrefs.setString(tKey, tStringValue))
             .thenAnswer((_) async => tResponse);
+
         // WHEN
         final result = await sharedPrefsFacade.saveData(
           key: tKey,
           value: tStringValue,
         );
+
         // THEN
         verify(() => mockSharedPrefs.setString(tKey, tStringValue)).called(1);
         expect(result, tResponse);
@@ -51,11 +53,13 @@ void main() {
         // GIVEN
         const tResponse = true;
         when(() => mockSharedPrefs.setInt(tKey, tIntValue)).thenAnswer((_) async => tResponse);
+
         // WHEN
         final result = await sharedPrefsFacade.saveData(
           key: tKey,
           value: tIntValue,
         );
+
         // THEN
         verify(() => mockSharedPrefs.setInt(tKey, tIntValue)).called(1);
         expect(result, tResponse);
@@ -70,11 +74,13 @@ void main() {
         const tResponse = true;
         when(() => mockSharedPrefs.setDouble(tKey, tDoubleValue))
             .thenAnswer((_) async => tResponse);
+
         // WHEN
         final result = await sharedPrefsFacade.saveData(
           key: tKey,
           value: tDoubleValue,
         );
+
         // THEN
         verify(() => mockSharedPrefs.setDouble(tKey, tDoubleValue)).called(1);
         expect(result, tResponse);
@@ -88,11 +94,13 @@ void main() {
         // GIVEN
         const tResponse = true;
         when(() => mockSharedPrefs.setBool(tKey, tBoolValue)).thenAnswer((_) async => tResponse);
+
         // WHEN
         final result = await sharedPrefsFacade.saveData(
           key: tKey,
           value: tBoolValue,
         );
+
         // THEN
         verify(() => mockSharedPrefs.setBool(tKey, tBoolValue)).called(1);
         expect(result, tResponse);
@@ -107,11 +115,13 @@ void main() {
         const tResponse = true;
         when(() => mockSharedPrefs.setStringList(tKey, tStringList))
             .thenAnswer((_) async => tResponse);
+
         // WHEN
         final result = await sharedPrefsFacade.saveData(
           key: tKey,
           value: tStringList,
         );
+
         // THEN
         verify(() => mockSharedPrefs.setStringList(tKey, tStringList)).called(1);
         expect(result, tResponse);
@@ -126,17 +136,39 @@ void main() {
       () async {
         // GIVEN
         when(() => mockSharedPrefs.setString(tKey, tStringValue)).thenThrow(tError);
+
         // WHEN
         final call = sharedPrefsFacade.saveData(
           key: tKey,
           value: tStringValue,
         );
+
         // THEN
         await expectLater(
           () => call,
           throwsA(tError.localErrorToCacheException()),
         );
         verify(() => mockSharedPrefs.setString(tKey, tStringValue)).called(1);
+        verifyNoMoreInteractions(mockSharedPrefs);
+      },
+    );
+
+    test(
+      'should throw UnsupportedError when value is not supported',
+      () async {
+        // GIVEN
+        final value = [1, 2, 3];
+
+        // WHEN
+        final call = sharedPrefsFacade.saveData(
+          key: tKey,
+          value: value,
+        );
+        // THEN
+        await expectLater(
+          () => call,
+          throwsException,
+        );
         verifyNoMoreInteractions(mockSharedPrefs);
       },
     );
@@ -149,8 +181,10 @@ void main() {
         // GIVEN
         const tResponse = tStringValue;
         when(() => mockSharedPrefs.getString(tKey)).thenReturn(tResponse);
+
         // WHEN
         final result = sharedPrefsFacade.restoreData<String>(tKey);
+
         // THEN
         verify(() => mockSharedPrefs.getString(tKey)).called(1);
         expect(result, tResponse);
@@ -163,8 +197,10 @@ void main() {
       () async {
         // GIVEN
         when(() => mockSharedPrefs.getInt(tKey)).thenReturn(tIntValue);
+
         // WHEN
         final result = sharedPrefsFacade.restoreData<int>(tKey);
+
         // THEN
         verify(() => mockSharedPrefs.getInt(tKey)).called(1);
         expect(result, tIntValue);
@@ -177,8 +213,10 @@ void main() {
       () async {
         // GIVEN
         when(() => mockSharedPrefs.getDouble(tKey)).thenReturn(tDoubleValue);
+
         // WHEN
         final result = sharedPrefsFacade.restoreData<double>(tKey);
+
         // THEN
         verify(() => mockSharedPrefs.getDouble(tKey)).called(1);
         expect(result, tDoubleValue);
@@ -191,8 +229,10 @@ void main() {
       () async {
         // GIVEN
         when(() => mockSharedPrefs.getBool(tKey)).thenReturn(tBoolValue);
+
         // WHEN
         final result = sharedPrefsFacade.restoreData<bool>(tKey);
+
         // THEN
         verify(() => mockSharedPrefs.getBool(tKey)).called(1);
         expect(result, tBoolValue);
@@ -205,8 +245,10 @@ void main() {
       () async {
         // GIVEN
         when(() => mockSharedPrefs.getStringList(tKey)).thenReturn(tStringList);
+
         // WHEN
         final result = sharedPrefsFacade.restoreData<List<String>>(tKey);
+
         // THEN
         verify(() => mockSharedPrefs.getStringList(tKey)).called(1);
         expect(result, tStringList);
@@ -231,6 +273,17 @@ void main() {
         verifyNoMoreInteractions(mockSharedPrefs);
       },
     );
+
+    test(
+      'should throw UnsupportedError when the generic value is not supported',
+      () async {
+        expect(
+          () => sharedPrefsFacade.restoreData<List<int>>(tKey),
+          throwsException,
+        );
+        verifyNoMoreInteractions(mockSharedPrefs);
+      },
+    );
   });
 
   group('clearAll', () {
@@ -240,8 +293,10 @@ void main() {
         // GIVEN
         const tResponse = true;
         when(() => mockSharedPrefs.clear()).thenAnswer((_) async => tResponse);
+
         // WHEN
         final result = await sharedPrefsFacade.clearAll();
+
         // THEN
         verify(() => mockSharedPrefs.clear()).called(1);
         expect(result, tResponse);
@@ -255,8 +310,10 @@ void main() {
       () async {
         // GIVEN
         when(() => mockSharedPrefs.clear()).thenThrow(tError);
+
         // WHEN
         final call = sharedPrefsFacade.clearAll();
+
         // THEN
         await expectLater(
           () => call,
@@ -275,8 +332,10 @@ void main() {
         // GIVEN
         const tResponse = true;
         when(() => mockSharedPrefs.remove(tKey)).thenAnswer((_) async => tResponse);
+
         // WHEN
         final result = await sharedPrefsFacade.clearKey(key: tKey);
+
         // THEN
         verify(() => mockSharedPrefs.remove(tKey)).called(1);
         expect(result, tResponse);
@@ -290,8 +349,10 @@ void main() {
       () async {
         // GIVEN
         when(() => mockSharedPrefs.remove(tKey)).thenThrow(tError);
+
         // WHEN
         final call = sharedPrefsFacade.clearKey(key: tKey);
+
         // THEN
         await expectLater(
           () => call,
