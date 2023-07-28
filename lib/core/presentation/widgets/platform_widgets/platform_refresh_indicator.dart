c02ecf8006ee12ffce2f623415b1e1ab9d14eca3
Ahmed Elsayed <ahmedelsaayid@gmail.com>
2023-07-28 20:29:31 +0300

    Update PlatformRefreshIndicator

--- lib/core/presentation/widgets/platform_widgets/platform_refresh_indicator.dart
+++ lib/core/presentation/widgets/platform_widgets/platform_refresh_indicator.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../../../core_features/theme/presentation/utils/colors/custom_colors.dart';
 import 'platform_base_widget.dart';
 
 class PlatformRefreshIndicator extends PlatformBaseWidget<RefreshIndicator, CustomScrollView> {
@@ -13,17 +14,17 @@ class PlatformRefreshIndicator extends PlatformBaseWidget<RefreshIndicator, Cust
   });
 
   final Key? widgetKey;
+  final ScrollController? scrollController;
   final Future<void> Function() onRefresh;
   final List<Widget> slivers;
-  final ScrollController? scrollController;
 
   @override
   RefreshIndicator createMaterialWidget(BuildContext context) {
     return RefreshIndicator(
       key: widgetKey,
       onRefresh: onRefresh,
+      color: customColors(context).loadingIndicatorColor,
       child: CustomScrollView(
-        key: widgetKey,
         controller: scrollController,
         slivers: slivers,
       ),
