cb0d1d5c351c49e9d0c6bc81fdf7b4f8c97d6adc
Ahmed Elsayed <ahmedelsaayid@gmail.com>
2023-10-12 17:54:31 +0300

    remove unnecessary material wrapper

--- lib/features/map/presentation/widgets/map_search_menu_item.dart
+++ lib/features/map/presentation/widgets/map_search_menu_item.dart
@@ -15,35 +15,33 @@ class MapSearchMenuItem extends StatelessWidget {
     final title = placeAutocomplete.mainText;
     final subTitle = placeAutocomplete.secondaryText;
 
-    return Material(
-      child: ListTile(
-        horizontalTitleGap: 0,
-        leading: const Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              Icons.place,
-              size: Sizes.icon24,
-            ),
-          ],
-        ),
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              title,
-              style: TextStyles.f16(context).copyWith(fontWeight: FontStyles.fontWeightBold),
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
-            Text(
-              subTitle,
-              style: TextStyles.f14(context),
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
-          ],
-        ),
+    return ListTile(
+      horizontalTitleGap: 0,
+      leading: const Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.place,
+            size: Sizes.icon24,
+          ),
+        ],
+      ),
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: TextStyles.f16(context).copyWith(fontWeight: FontStyles.fontWeightBold),
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+          ),
+          Text(
+            subTitle,
+            style: TextStyles.f14(context),
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ],
       ),
     );
   }
