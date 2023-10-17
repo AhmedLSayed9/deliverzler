f7ea90228a77cbe91b979ebefe3506d97e683e8f
Ahmed Elsayed <ahmedelsaayid@gmail.com>
2023-10-17 21:36:08 +0300

    Update GoogleMaps ErrorInterceptor

--- lib/core/infrastructure/network/google_map_api/interceptors/error_interceptor.dart
+++ lib/core/infrastructure/network/google_map_api/interceptors/error_interceptor.dart
@@ -26,14 +26,10 @@ class ErrorInterceptor extends Interceptor {
 
   @override
   Future<void> onError(DioError err, ErrorInterceptorHandler handler) async {
-    // If your api throws custom statusCode i.e 400 for business errors, you can do:
-    // `if (statusCode == 400 || err.error is _RejectError)`
-    // Note: statusCode is 400 when thrown from backend, error is _RejectError when thrown from onResponse.
-
-    if (err.error is RejectError) {
+    if (err.response?.statusCode == 400 || err.error is RejectError) {
       final response = err.response!;
       final data = response.data as Map<dynamic, dynamic>;
-      final error = data['status'];
+      final error = data['error_message'] ?? data['status'];
       final newErr = err.copyWith(
         response: Response(
           statusCode: 400,
